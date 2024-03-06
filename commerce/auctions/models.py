268f2bc33af80b813fec384a98a3e147b42a60aa
lina <laalhuri@gmail.com>
2024-03-06 12:11:26 +0300

    refact: update bid model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,13 +5,7 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Bid(models.Model):
-    bid = models.DecimalField(decimal_places=2,max_digits=13, blank=True)
-    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
-    #auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f'{self.bid}$ by {self.bidder}'
 
 class Category(models.Model):
     category = models.CharField(max_length=64)
@@ -25,13 +19,22 @@ class Auction(models.Model):
     description = models.TextField()
     image = models.URLField(null=True, blank=True, default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTelVna9__Qwt9GifGdE0R4FmsiTmZjoSE1vnC4LXdgozvqbjiOGufuXrladHL7nXowTt4&usqp=CAU")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    bid = models.ForeignKey(Bid, on_delete=models.CASCADE,related_name="biddings", blank=True,null=True)
     start_price = models.DecimalField(decimal_places=2,max_digits=13, default=1)
     status = models.BooleanField(default = True)
 
     def __str__(self):
         return f"{self.title}: {self.description} belongs to {self.category} starting with {self.start_price}"
+    
+class Bid(models.Model):
+    bid = models.DecimalField(decimal_places=2,max_digits=13, blank=True,default=0)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="biddings", blank=True, null=True)
 
+
+    def __str__(self):
+        return f'{self.bid}$ by {self.bidder}'
+        
+        
 class Comment(models.Model):
     content = models.TextField()
     commentor = models.ForeignKey(User, on_delete=models.CASCADE)
